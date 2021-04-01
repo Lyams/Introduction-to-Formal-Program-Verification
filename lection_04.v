3e2760c2448e6381a015ed8240e443069f4a7860
Ilya <lyamsh@yandex.ru>
2021-04-01 18:52:23 +0300

    Realise or_introl_inj function in 4 lection

--- lection_04.v
+++ lection_04.v
@@ -11,3 +11,21 @@ Definition succ_inj (n m : nat) :
     with
     | erefl => erefl n
     end.
+
+Definition succ_inj' (n m : nat) :
+  n.+1 = m.+1 -> n = m
+:=  fun Sn_Sm => match Sn_Sm in _ = S q
+                    return n = q with
+                 | erefl => erefl n
+                  end.
+
+Definition or_inrol_inj (A B : Prop) (p1 p2 :A) :
+  or_introl p1 = or_introl p2 :> (A \/ B) -> p1 = p2
+:= fun eq =>
+    match
+      eq in (_ = oil2)
+      return (p1 = if oil2 is or_introl p2'
+                    then p2' else p2)
+    with
+    | erefl => erefl p1
+    end.
