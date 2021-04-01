f96331e717de90df16319817514dff83d9a0dc86
Ilya <lyamsh@yandex.ru>
2021-04-01 05:09:42 +0300

    add example with syll

--- by_video_01.v
+++ by_video_01.v
@@ -1,3 +1,20 @@
 (* По видео
 Кузнецов С.Л. Coq: построение и проверка математических доказательств на компьютере (22.04.2020)
-: https://www.youtube.com/watch?v=D6sdE3FVlcY*)
+: https://www.youtube.com/watch?v=D6sdE3FVlcY *)
+
+Variable A B C : Prop.
+
+Theorem syll : (A -> B) -> ((B -> C) -> (A -> C)).
+Proof.
+intro.
+intro.
+intro.
+apply H0.
+apply H.
+assumption.
+Qed.
+
+Theorem syll' : (A -> B) -> ((B -> C) -> (A -> C)).
+Proof.
+auto.
+Qed.
