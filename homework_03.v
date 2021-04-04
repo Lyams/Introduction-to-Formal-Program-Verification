138c5053980b1f5dfa9901a391e042a75f9bc6d3
Ilya <lyamsh@yandex.ru>
2021-04-04 03:34:02 +0300

    end section equality in third homework

--- homework_03.v
+++ homework_03.v
@@ -129,35 +129,39 @@ Definition if_neg :
 
 Definition compA (f : A -> B) (g : B -> C) (h : C -> D) :
   (h \o g) \o f = h \o (g \o f)
-:= .
-
+:= erefl.
+Print ex_intro.
 
 (** [=1] stands for extensional equality on unary functions,
     i.e. [f =1 g] means [forall x, f x = g x].
     This means it's an equivalence relation, i.e. it's reflexive, symmetric and transitive.
     Let us prove a number of facts about [=1]. *)
-
-
+Print erefl.
+Print eq_refl.
 (** * Exercise: Reflexivity *)
 Definition eqext_refl :
   forall (f : A -> B), f =1 f
-:= replace_with_your_solution_here.
-
+:= fun x y => erefl.
+About eqext_refl.
 (** * Exercise: Symmetry *)
 Definition eqext_sym :
   forall (f g : A -> B), f =1 g -> g =1 f
-:= replace_with_your_solution_here.
+:= fun _ _ z r => match z r with
+    erefl => erefl end.
 
 (** * Exercise: Transitivity *)
 Definition eqext_trans :
   forall (f g h : A -> B), f =1 g -> g =1 h -> f =1 h
-:= replace_with_your_solution_here.
+:= fun f g h f_g g_h a => match g_h a : g a = h a in _ = b return f a = b with
+    | erefl => f_g a end.
+
 
 (** * Exercise: left congruence *)
 Definition eq_compl :
   forall (f g : A -> B) (h : B -> C),
     f =1 g -> h \o f =1 h \o g
-:= replace_with_your_solution_here.
+:= fun f g h f_g a => match f_g a : f a = g a 
+    in _ = c return _ = h c with erefl => erefl end.
 
 (** * Exercise: right congruence *)
 Definition eq_compr :
