23b0037ad3d15adeceb341473eddd5d8aaaf2785
Ilya <lyamsh@yandex.ru>
2021-04-06 00:55:08 +0300

    realize factorial function (exercise) in selfwork

--- self_work_01.v
+++ self_work_01.v
@@ -98,5 +98,104 @@ Proof. simpl. reflexivity. Qed.
 Example test_andb34: (andb3 true true false) = false.
 Proof. simpl. reflexivity. Qed.
 
+Check true.
+Check negb : bool -> bool.
+Fail Check negb : bool.
+
+Inductive rgb : Type :=
+  | red
+  | green
+  | blue.
+Inductive color : Type :=
+  | black
+  | white
+  | primary (p : rgb).
+Definition monochrome (c : color) : bool :=
+  match c with
+  | black => true
+  | white => true
+  | primary p => false
+  end.
+Definition isred (c : color) : bool :=
+  match c with
+  | black => false
+  | white => false
+  | primary red => true
+  | primary _ => false
+  end.
+
+Module Playground.
+  Definition b : rgb := blue.
+End Playground.
+Definition b : bool := true.
+Check Playground.b : rgb.
+Check b : bool.
+
+Module TuplePlayground.
+
+Inductive bit : Type := B0 | B1.
+Inductive nybble : Type := bits (b0 b1 b2 b3 : bit).
+Check (bits B1 B0 B0 B0) : nybble.
+
+Definition all_zero (nb : nybble) : bool
+:= match nb with
+  | (bits B0 B0 B0 B0) => true
+  | _ => false
+  end.
+
+Compute (all_zero (bits B1 B0 B1 B0)).
+Compute (all_zero (bits B0 B0 B0 B0)).
+
+End TuplePlayground.
+Check (S (S (S (S O)))).
+Definition minustwo (n : nat) : nat
+:= match n with
+   | O => O
+   | S O => O
+   | S (S n') => n'
+   end.
+
+Compute (minustwo 10).
+Check S : nat -> nat.
+Check pred : nat -> nat.
+Check minustwo : nat -> nat.
+
+Fixpoint evenb (n : nat) : bool
+:= match n with
+  | O => true
+  | S O => false
+  | S ( S n') => evenb n'
+  end.
+
+Definition oddb (x : nat) : bool := negb (evenb x).
+Example test_oddb1: oddb 1 = true.
+Proof. simpl. reflexivity. Qed.
+Example test_oddb2: oddb 4 = false.
+Proof. simpl. reflexivity. Qed.
+Example test_evenb1: evenb 1 = false.
+Proof. simpl. reflexivity. Qed.
+Example test_evenb2: evenb 4 = true.
+Proof. simpl. reflexivity. Qed.
+
+Fixpoint exp (base power : nat) : nat
+:= match power with
+   | O => S O
+   | S p => mult base (exp base p)
+   end.
+
+(*Exercise: 1 star, standard (factorial)*)
+Fixpoint factorial (n:nat) : nat
+:= match n with
+  | O => 1
+  | (S n') => n * (factorial n')
+  end.
+Example test_factorial1: (factorial 3) = 6.
+Proof. simpl. reflexivity. Qed.
+Example test_factorial2: (factorial 5) = (mult 10 12).
+Proof. simpl. reflexivity. Qed.
+Compute (factorial 5).
+
+
+
 
 End My.
