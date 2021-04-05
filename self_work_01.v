3813351d0b0abc5f1b8b86808206143e5e4370e9
Ilya <lyamsh@yandex.ru>
2021-04-06 00:24:23 +0300

    complite Exercise:1 in self work 1

--- self_work_01.v
+++ self_work_01.v
@@ -83,4 +83,20 @@ Proof. simpl. reflexivity. Qed.
 Example test_nandb4: (nandb true true) = false.
 Proof. simpl. reflexivity. Qed.
 
+(* Exercise: 1 star, standard (andb3) *)
+Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool
+:=  match b1 with
+  | true => b2 && b3
+  | false => false
+  end.
+Example test_andb31: (andb3 true true true) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_andb32: (andb3 false true true) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_andb33: (andb3 true false true) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_andb34: (andb3 true true false) = false.
+Proof. simpl. reflexivity. Qed.
+
+
 End My.
