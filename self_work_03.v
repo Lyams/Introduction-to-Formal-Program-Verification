c1c11adc92590c3d887d504cdcf11882e682ecd1
Ilya L <lyamsh@yandex.ru>
2021-04-22 15:14:42 +0300

    end part 2.3  Two so-called paradoxes

--- self_work_03.v
+++ self_work_03.v
@@ -96,9 +96,25 @@ Section R_sym_trans.
 Variables (D : Type) (R : D -> D -> Prop).
 Hypothesis R_sym : forall x y, R x y -> R y x.
 Hypothesis R_trans : forall x y z, R x y -> R y z -> R x z.
+
 Lemma refl_if : forall x : D, (exists y, R x y) -> R x x.
-Proof. move=> x [y Rxy].
+Proof. 
+(* move=> x; case=> y Rxy. *) move=> x [y Rxy].
 by apply: R_trans _ (R_sym _ y _).
 Qed.
 
-End R+sym_trans.
+End R_sym_trans.
+
+Section Smullyan_drinker.
+Variables (D : Type)(P : D -> Prop).
+Hypothesis (d : D) (EM : forall A, A\/~A).
+
+Lemma drinker : exists x, P x -> forall y, P y.
+Proof.
+case: (EM (exists y, ~P y)) => [[y notPy]| nonotPy];
+  first by exists y.
+exists d => _ y; case: (EM (P y)) => // notPy.
+by case: nonotPy; exists y. Qed.
+
+End Smullyan_drinker.
+
